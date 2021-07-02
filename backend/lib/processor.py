8144919a98377119021c3266cb5a55144e9654fb
andrea1diaz <andrea.diaz.gardini@gmail.com>
2021-07-02 15:36:54 -0500

    blocks read func

--- backend/lib/processor.py
+++ backend/lib/processor.py
@@ -7,30 +7,30 @@ class Processor:
     def __init__(self):
         files = []
 
-    for file in os.listdir(filepath):
-        files.append(os.path.splitext(file)[0])
+        for file in os.listdir(filepath):
+            files.append(os.path.splitext(file)[0])
 
-    number_of_docs = len(files);
-    docs_per_block = number_of_docs / float(np.log(number_of_docs))
+        number_of_docs = len(files);
+        docs_per_block = number_of_docs / float(np.log(number_of_docs))
 
-    z = 0
+        z = 0
 
-    for i in range (0, number_of_docs, int(docs_per_block)):
-        if i + docs_per_block < number_of_docs:
-            size = docs_per_block
+        for i in range (0, number_of_docs, int(docs_per_block)):
+            if i + docs_per_block < number_of_docs:
+                size = docs_per_block
 
-        else:
-            size = number_of_docs - i - 1;
+            else:
+                size = number_of_docs - i - 1;
 
-        
-        block = Block (i)
-        
-        for j in range (0, int(size)):
-            block.insert_doc(files[z])
-            # here the inverted index in doc
-            z += 1
+            
+            block = Block (i)
+            
+            for j in range (0, int(size)):
+                block.insert_doc(files[z])
+                # here the inverted index in doc
+                z += 1
 
-        block.write_block (number_of_docs)
+            block.write_block (number_of_docs)
 
 
 
@@ -61,6 +61,12 @@ class Block:
         with open (str(self.block_id) + '.json', 'w') as file:
             json.dump(data, file, ensure_ascii=False)
 
+    
+    def read_block(self, block_id: int):
+        with open(str(block_id) + '.json') as file:
+            data = json.load(file)
 
+            self.doc = data["docs"]
+            self.words = data["words]
 
 
